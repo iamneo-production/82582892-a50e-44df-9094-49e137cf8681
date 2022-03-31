4552aa334141ca5b544af8dca95c92333170cb81
ramcharan1324 <72651469+ramcharan1324@users.noreply.github.com>
2022-04-01 00:36:00 +0530

    Create Enrolledcourse.js

---
+++ reactapp/src/components/pages/Enrolledcourse.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Enrolledcourse() {
+  return (
+    <div className="page-heading">
+      <h1>Enrolled course</h1>
+    </div>
+  );
+}
+
+export default Enrolledcourse;
