bb1cc082cf0adb53691fdf62268fc1d77d784eb7
nehamittakori <97791536+nehamittakori@users.noreply.github.com>
2022-03-31 23:23:32 +0530

    Create MenuList.js

---
+++ reactapp/src/components/User layouts/Navbar/MenuList.js
@@ -0,0 +1,14 @@
+export const MenuList = [
+  {
+    title: "Academy",
+    url: "academy",
+  },
+  {
+    title: "Enrolled course",
+    url: "enrolledcourse",
+  },
+  {
+    title: "Logout",
+    url: "/login",
+  },
+];
