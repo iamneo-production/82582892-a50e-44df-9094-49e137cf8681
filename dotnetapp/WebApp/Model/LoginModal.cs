3a1710781bf65f90442d8f3ab20d80146e527cf9
Yamuna-Devi10 <76040231+Yamuna-Devi10@users.noreply.github.com>
2022-04-01 22:26:42 +0530

    Create LoginModal.cs

---
+++ dotnetapp/WebApp/Model/LoginModal.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Academy_Backend.Model
+{
+    public partial class LoginModal
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
