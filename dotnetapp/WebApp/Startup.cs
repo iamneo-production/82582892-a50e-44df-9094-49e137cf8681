0c42337fe571c94d0d58f92ac6217ccf04840efa
IshwaryaLinga <71553271+IshwaryaLinga@users.noreply.github.com>
2022-04-01 22:17:20 +0530

    Update Startup.cs

--- dotnetapp/WebApp/Startup.cs
+++ dotnetapp/WebApp/Startup.cs
@@ -1,18 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Academy_Backend.Model;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace WebApp
+namespace Academy_Backend
 {
     public class Startup
     {
@@ -26,22 +28,27 @@ namespace WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var Academy_Con = "Data Source=PARTHA-PC\\SQLEXPRESS;Initial Catalog=Academy_Backend;Integrated Security=True";
+            services.AddDbContext<Academy_BackendContext>(o => o.UseSqlServer(Academy_Con));
 
+            services.AddCors();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApp", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Academy_Backend", Version = "v1" });
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options =>
+                options.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader());
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApp v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Academy_Backend v1"));
             }
 
             app.UseHttpsRedirection();
